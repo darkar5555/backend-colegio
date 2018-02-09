4b0ffb0fa5b76b80722c9c9cf033ad7bb23fa967
darkar5555 <rodrigo.castro@ucsp.edu.pe>
2018-02-09 16:31:41 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// requires
+
+var express = require('express');
+var mongoose = require('mongoose');
+
+
+//Inicializar variables
+var app = express();
+
+
+//Conexion a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/colegioDB', (err, res)=>{
+    if (err) throw err;
+    console.log('Base de datos: online');
+});
+
+
+//Rutas 
+app.get('/', (req, res, next)=>{
+
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    });
+
+});
+
+
+//Escuchar peticiones
+app.listen(3000, ()=>{
+    console.log('Express server corriendo en el pueto 3000 online')
+});
